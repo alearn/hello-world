e7cb94dd184fc6058e2fdb33ce5cf7fea52493f5
kai <zk@htu.cn>
2019-09-03 06:48:22 +0800

    array

--- python/d5_exercise_array.py
+++ python/d5_exercise_array.py
@@ -1,5 +1,18 @@
-d5_0=[0,1,2,3,4,5,6,7,8,9]
+d5_list=[0,1,2,3,4,5,6,7,8,9]
 #将数组翻转
-d5_1=d5_0.reverse()
-print(d5_1)
+d5_list.reverse() 
+print(d5_list)
 print('\n')
+
+#翻转后的数组拼接成字符串
+s=''
+for i in range(0,10):
+    d5_list[i]=str(d5_list[i])
+d5_string=s.join(d5_list)
+print(d5_string)
+
+#字符串切片
+
+d5_string_1=d5_string[2:8]
+print(d5_string_1)
+
