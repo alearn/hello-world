3141d5fa0da9fecde166cad31cacbc68998ab1c3
kevin <zk@htu.cn>
2019-10-08 19:44:39 +0800

    bs1-5

--- python/bs1-3.py
+++ python/bs1-3.py
@@ -10,15 +10,16 @@ import random
 headers = {
     'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:69.0) Gecko/20100101 Firefox/69.0'
     }
-#url = 'http://www.htu.edu.cn/8954/list.htm'
-url = 'https://haokan.baidu.com/videoui/page/pc/search?query=%E9%87%8F%E5%AD%90%E6%95%85%E4%BA%8B%E4%BC%9A%E4%B9%9D'
+url = 'http://www.htu.edu.cn/8954/list.htm'
+#url = 'https://haokan.baidu.com/videoui/page/pc/search?query=%E9%87%8F%E5%AD%90%E6%95%85%E4%BA%8B%E4%BC%9A%E4%B9%9D'
 r = requests.get(url=url,headers=headers)
 soup = BeautifulSoup(r.content,'lxml')
-print (soup.prettify())
+#print (soup.prettify())
 #print (soup.title)
 #print (soup.title.string)
-#news_div = soup.find(attrs={"class":"list_right"})
-#news_a = news_div.findAll(attrs={"class":"Article_Title"})
-#print (news_a)
-#for news in news_a:
-#    print (news.string)
+news_div = soup.find(attrs={"class":"list_right"})
+news_a = news_div.findAll(attrs={"class":"Article_Title"})
+
+print (news_a)
+for news in news_a:
+    print (news.string)
